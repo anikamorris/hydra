2a39c854de13ee305e8ed22c0448e79d30f8e4fb
Anika Morris <anikamorris11@gmail.com>
2020-11-30 18:15:35 -0600

    started adding navigation

--- water-tracker/Views/FirstView.swift
+++ water-tracker/Views/FirstView.swift
@@ -8,37 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isShowingDetailView = false
+    
     var body: some View {
-        VStack {
-            Image("hydra-logo")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 200, height: 90, alignment: .center)
-            Spacer()
-            Image("water-drop")
-                .resizable()
-                .frame(width: 200, height: 200, alignment: .center)
-                .aspectRatio(contentMode: .fit)
-            Text("Your body needs water!")
-                .fontWeight(.semibold)
-                .foregroundColor(.skyBlue)
-                .font(.system(size: 20))
-            Text("Track your daily water intake in just a few taps.")
-                .font(.system(size: 17))
-                .foregroundColor(.secondary)
-                .multilineTextAlignment(.center)
-            Spacer()
-            Button("Get Started") {
-                print("starting")
-            }
-                .foregroundColor(.white)
-                .padding()
-                .frame(width: 250, height: 50, alignment: .center)
-                .background(Color.skyBlue)
-                .cornerRadius(8)
+        NavigationView {
+            VStack {
+                Image("hydra-logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 90, alignment: .center)
+                    .offset(x: 0, y: -50.0)
+                Spacer()
+                Image("water-drop")
+                    .resizable()
+                    .frame(width: 200, height: 200, alignment: .center)
+                    .aspectRatio(contentMode: .fit)
+                    .offset(x: 0, y: -40.0)
+                Text("Your body needs water!")
+                    .fontWeight(.semibold)
+                    .foregroundColor(.skyBlue)
+                    .font(.system(size: 20))
+                    .offset(x: 0, y: -30.0)
+                Text("Track your daily water intake.")
+                    .font(.system(size: 17))
+                    .foregroundColor(.secondary)
+                    .offset(x: 0, y: -30.0)
+                Spacer()
+                NavigationLink(destination: HomeView(), isActive: $isShowingDetailView) { EmptyView() }
+                Button("Get Started") {
+                    self.isShowingDetailView = true
+                }
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: 250, height: 50, alignment: .center)
+                    .background(Color.skyBlue)
+                    .cornerRadius(8)
 
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
--- water-tracker/Views/WaterTrackerView.swift
+++ water-tracker/Views/WaterTrackerView.swift
@@ -8,10 +8,21 @@
 import SwiftUI
 
 struct WaterTrackerView: View {
+    @State private var completionAmount: CGFloat = 0.0
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
     var body: some View {
-        Rectangle()
-            .trim(from: 0, to: 0.5)
-            .fill(Color.medLightBlue)
+        Circle()
+            .trim(from: 0, to: completionAmount)
+            .stroke(Color.red, lineWidth: 20)
+            .frame(width: 200, height: 200)
+            .rotationEffect(.degrees(-90))
+            .onReceive(timer) { _ in
+                withAnimation {
+                    guard self.completionAmount < 1 else { return }
+                    self.completionAmount += 0.2
+                }
+            }
     }
 }
 
